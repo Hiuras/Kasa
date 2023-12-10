40bbfca830f52b40a18ea0c98f66894397496195
Hiuras <137842225+Hiuras@users.noreply.github.com>
2023-12-10 22:17:40 +0100

    Update Main Page

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,13 +1,25 @@
-import '../styles/Cards.css'
-import '../logements.json'
+import '../styles/Cards.css';
+import logement from '../logements.json';
 
 function Cards() {
-    return ( 
-        <div className="Cards-background">
-            <section>
-            </section>
+  return (
+    <div className="Cards-background">
+      <section>
+        <div className='cards-Main'>
+          <section className='logement'>
+            <div className="card-container">
+              {logement.map((item, index) => (
+                <div className="card" key={index}>
+                  <h3>{item.title}</h3>
+                  <img src={item.cover} alt='image de la maison' />
+                </div>
+              ))}
+            </div>
+          </section>
         </div>
-    )
+      </section>
+    </div>
+  );
 }
 
-export default Cards
+export default Cards;
