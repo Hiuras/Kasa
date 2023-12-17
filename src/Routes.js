4af408829bb3b681d6b9ebf2b8e1ae2f888ace8f
Hiuras <137842225+Hiuras@users.noreply.github.com>
2023-12-17 15:52:27 +0100

    Ajout Error 404

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import App from './components/App';
 import About from './components/About';
+import Error from './components/Error';
 
 const AppRoutes = () => {
   return (
@@ -8,6 +9,7 @@ const AppRoutes = () => {
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/about" element={<About />} />
+        <Route path='/error' element={<Error />} />
       </Routes>
     </Router>
   );
